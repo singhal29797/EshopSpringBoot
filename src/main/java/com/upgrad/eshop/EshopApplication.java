ae135cc36ecc2e658c80a81d6e63e810353900b6
Shivam <singhal29797shivam@gmail.com>
2020-09-18 19:43:08 +0530

    First Commit

---
+++ src/main/java/com/upgrad/eshop/EshopApplication.java
@@ -0,0 +1,15 @@
+package com.upgrad.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		SpringApplication.run(EshopApplication.class, args);
+
+	}
+
+}
